639f72e2c4992ec761373612fba757a859488bb8
DanielCosta12 <danieldossantosdacosta@gmail.com>
2022-08-20 20:24:09 -0300

    adicionando nova forma

--- script.js
+++ script.js
@@ -6,4 +6,15 @@ verificarPalindromo = (string) =>{
    
 }
 
-console.log(verificarPalindromo("ana"));
+verificarPalindromo2 = (string) =>{
+    if (!string) return "String inexistente";
+    for(let i = 0; i < string.length / 2; i++){
+        if(string[i] !== string[string.length -1 - i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(verificarPalindromo("abba"));
+console.log(verificarPalindromo2("ama"));
